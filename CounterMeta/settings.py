2c2adafb470c1daec2a10861eb14f5768d4de4f6
IClaudius <cap8013@rit.edu>
2013-11-05 20:07:44 -0500

    Added small SQL database from build. Username is RM

--- CounterMeta/settings.py
+++ CounterMeta/settings.py
@@ -11,14 +11,22 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'postgres2',                      # Or path to database file if using sqlite3.
-        'USER': 'postgres',                      # Not used with sqlite3.
-        'PASSWORD': 'asdfas2',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
-    }
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#        'NAME': 'postgres2',                      # Or path to database file if using sqlite3.
+#        'USER': 'postgres',                      # Not used with sqlite3.
+#        'PASSWORD': 'asdfas2',                  # Not used with sqlite3.
+#        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+#        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
+#    }
+'default': {
+	'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+	'NAME': 'CounterMetaSite',                      # Or path to database file if using sqlite3.
+	'USER': '',                      # Not used with sqlite3.
+	'PASSWORD': '',                  # Not used with sqlite3.
+	'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+	'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+	}
 }
 
 # Local time zone for this installation. Choices can be found here:
